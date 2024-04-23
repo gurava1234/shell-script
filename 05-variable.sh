1bdb9f2b9ccd9f79a266a9e4f4b4c9c7a7073f2a
gurava1234 <bachuguravareddy47@gmail.com>
2024-04-23 18:50:35 +0530

    i

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "please enter your username"
-read USERNAME #the value entered above automatically attached to USERNAME variable
+read -s USERNAME #the value entered above automatically attached to USERNAME variable
 
 echo "username is:$USERNAME" 
