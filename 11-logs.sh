20b356c86c5748ab738ad995bb53824cffeacc53
gurava1234 <bachuguravareddy47@gmail.com>
2024-04-24 11:39:03 +0530

    i

---
+++ 11-logs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo"script name: $0"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "ERROR:: $2 ... failed"
+        exit 1
+    else
+    echo "$2 ...success"
+    fi
+
+}
+
+if [ $ID -ne 0]
+then 
+   echo "ERROR:: please run this script with root access"
+   exit 1 #you can give otherthan 0
+else 
+   echo "you are root user"
+fi
+ 
+ yum install mysql -y
+
+VALIDATE $ "Inatlling MySql"
+
+yum install git -y
+
+VALIDATE $question mark "Installing GIT"
