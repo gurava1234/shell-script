8ca233cfa3b62392ea6dbe6ff8dcb5131c36f283
gurava1234 <bachuguravareddy47@gmail.com>
2024-04-26 16:00:25 +0530

    roboshop shell scripts

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #this is a comment
+echo "please enter your username"
+read USERNAME #the value entered above automatically attached to USERNAME variable
 
 echo "hello shell script"
---
+++ 14-without-exit-status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+ls -ltr
+
+touch example.txt
+
+echo "before wrong command"
+
+lsfdgd
+
+echo "after wrong command"
+
+cd /temp
+
+cd /home/centos
+
