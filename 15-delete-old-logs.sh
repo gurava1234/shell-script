3cb86568e132d7157ddee97e6662f9e4561aa4b8
gurava1234 <bachuguravareddy47@gmail.com>
2024-04-28 11:15:10 +0530

    roboshop shell scripts

---
+++ 15-delete-old-logs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SOURCE_DIR=/tmp/shellscript-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m
+N="\e[0m
+
+if [ ! -d $SOURCE_DIR ] # ! denotes opposite
+
+then 
+    echo -e "$R source directory: $SOURCE_DIR does not exist. $N"
+
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "* .log")
+
+While IFS=read -r line
+do
+  echo "Deleting file: $line"
+
+  rm -rf $line
+
+done <<< $FILES_TO_DELETE
+
